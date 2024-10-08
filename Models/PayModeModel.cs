71699a1133cc69aa7ea6801e2b8324b167b739c6
Daniel Ortiz <danbedoya2001@gmail.com>
2024-10-08 07:44:33 -0500

    primer ejecucion del archivo

---
+++ Models/PayModeModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Supermarket_mvp.Models
+{
+    internal class PayModeModel
+    {
+        [DisplayName("Pay Mode Id")]
+        public int Id { get; set; }
+        [DisplayName("Pay Mode Name")]
+        [Required(ErrorMessage = "Pay Mode Name is Required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Pay Mode name must be between 3 and 50 characteres")]
+        public string Name { get; set; }
+        [DisplayName("Observation")]
+        [Required(ErrorMessage = "Pay Mode Observation is Required")]
+        [StringLength(200, MinimumLength =3 , ErrorMessage ="Pay Mode Observation must be between 3 and 200 characteres")]
+        public string Observation { get; set; }
+
+        public PayModeModel(int id, string name, string observation)
+        {
+            Id = id;
+            Name = name;
+            Observation = observation;
+        }
+    }
+}
--- obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Supermarket-mvp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff6586983e1620c64c129454a58b5a8f5638999e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Supermarket-mvp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
