a1a76def2941dec9b286361eb2afaf2479b5cd76
Daniel Ortiz <danbedoya2001@gmail.com>
2024-10-12 12:49:26 -0500

    Main View

--- Supermarket-mvp/Views/PayModeView.Designer.cs
+++ Supermarket-mvp/Views/PayModeView.Designer.cs
@@ -171,6 +171,7 @@
             // 
             DgPayMode.AllowUserToAddRows = false;
             DgPayMode.AllowUserToDeleteRows = false;
+            DgPayMode.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             DgPayMode.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             DgPayMode.Location = new Point(17, 79);
             DgPayMode.Name = "DgPayMode";
