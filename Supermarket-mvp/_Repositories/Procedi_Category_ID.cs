10de9565e7aa9660c1dd1352113f5a24e4070855
Daniel Ortiz Bedoya <danbedoya2001@gmail.com>
2024-10-13 16:27:30 -0500

    clase que complementa con la de connection para la llave foranea

---
+++ Supermarket-mvp/_Repositories/Procedi_Category_ID.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp._Repositories
+{
+    internal class Procedi_Category_ID
+    {
+        Connection connect = new Connection();
+        public DataTable Cargar_ID()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter("SP_CARGA_ID", connect.LlaveForanea());
+            adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            return dt;
+        }
+
+    }
+}
