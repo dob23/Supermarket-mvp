96c16245eb2f4e59c384c806754bd988dda37af8
Daniel Ortiz Bedoya <danbedoya2001@gmail.com>
2024-10-13 15:48:01 -0500

    Se crea el ProductModel

---
+++ Supermarket-mvp/Models/ProductsModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Models
+{
+    internal class ProductsModel
+    {
+        [DisplayName("Product Id")]
+        public int Product_Id { get; set; }
+
+        [DisplayName("Product Name")]
+        [Required(ErrorMessage = "Product Name is required")]
+        [StringLength(80, MinimumLength = 3, ErrorMessage = "Product Name must be between 3 and 80 Charaters")]
+        public string Name { get; set; }
+
+        [DisplayName("Product Price")]
+        [Required(ErrorMessage = "Product Price is required")]
+        public decimal Price { get; set; }
+
+        [DisplayName("Product Stock")]
+        [Required(ErrorMessage = "Product Stock is required")]
+        public int Stock { get; set; }
+
+        [DisplayName("Product Category_Id")]
+        [Required(ErrorMessage = "Product Categorie_Id is required")]
+        public int Category_Id { get; set; }
+
+
+    }
+}
