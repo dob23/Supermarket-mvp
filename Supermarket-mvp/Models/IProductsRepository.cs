177be606ef2b8f916a13ceece5a883bdcfa6a8bc
Daniel Ortiz Bedoya <danbedoya2001@gmail.com>
2024-10-13 15:48:57 -0500

    se crea el IProductsRepository

---
+++ Supermarket-mvp/Models/IProductsRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Models
+{
+    internal interface IProductsRepository
+    {
+        void ProductAdd(ProductsModel productsModel);
+        void ProductEdit(ProductsModel productsModel);
+        void ProductDelete(int id);
+        IEnumerable<ProductsModel> GetAll();
+        IEnumerable<ProductsModel> GetByValue(string value);
+    }
+}
