f95f5a9fc8001921640d29533d268fca7bc72ef8
Daniel Ortiz Bedoya <danbedoya2001@gmail.com>
2024-10-13 16:20:53 -0500

    se creo el ICategoryRepository

---
+++ Supermarket-mvp/Models/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supermarket_mvp.Models;
+namespace Supermarket_mvp.Models
+{
+    internal interface ICategoryRepository
+    {
+        void CategoryAdd(CategoryModel categoryModel);
+        void CategoryEdit(CategoryModel categoryModel);
+        void CategoryDelete(int id);
+        IEnumerable<CategoryModel> GetAll();
+        IEnumerable<CategoryModel> GetByValue(string value);
+    }
+}
