658b613e1c1ca18c45884b1f591f184844f3e3b3
Daniel Ortiz Bedoya <danbedoya2001@gmail.com>
2024-10-13 16:26:34 -0500

    se creo la clase connection (se usara para la clase de la llave foranea

---
+++ Supermarket-mvp/_Repositories/Connection.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp._Repositories
+{
+    internal class Connection
+    {
+        public SqlConnection LlaveForanea()
+        {
+            SqlConnection connect = new SqlConnection(ConfigurationManager.ConnectionStrings["SqlConnection"].ConnectionString);
+
+            if (connect.State == ConnectionState.Open)
+            {
+                connect.Close();
+            }
+            else
+            {
+                connect.Open();
+            }
+            return connect;
+        }
+    }
+}
