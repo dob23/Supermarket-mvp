423388ac17977d75edbe98d0da058c0420086f71
Daniel Ortiz Bedoya <danbedoya2001@gmail.com>
2024-10-13 16:06:45 -0500

    se crea el ICustomerRepository

---
+++ Supermarket-mvp/Models/ICustomerRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Models
+{
+    internal interface ICustomerRepository
+    {
+        void CustomersAdd(CustomerModel customerModel);
+        void CustomersEdit(CustomerModel customerModel);
+        void CustomersDelete(int id);
+        IEnumerable<CustomerModel> GetAll();
+        IEnumerable<CustomerModel> GetByValue(string value);
+    }
+}
