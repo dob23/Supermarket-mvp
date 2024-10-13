63bbb77e5a54f02bee870214be23b6d732de911f
Daniel Ortiz Bedoya <danbedoya2001@gmail.com>
2024-10-13 16:09:24 -0500

    se crea el CustomerView

---
+++ Supermarket-mvp/Views/CustomerView.Designer.cs
@@ -0,0 +1,478 @@
+﻿namespace Supermarket_mvp.Views
+{
+    partial class CustomerView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            tabControl1 = new TabControl();
+            tabPageCustomerList = new TabPage();
+            BtnClose = new Button();
+            BtnDelete = new Button();
+            BtnEdit = new Button();
+            BtnNew = new Button();
+            DgCustomer = new DataGridView();
+            BtnSearch = new Button();
+            TxtSearch = new TextBox();
+            label2 = new Label();
+            tabPageCustomerDetail = new TabPage();
+            BtnCancel = new Button();
+            BtnSave = new Button();
+            TxtEmail = new TextBox();
+            TxtPhone = new TextBox();
+            TxtBirthday = new TextBox();
+            TxtAddress = new TextBox();
+            TxtDocument = new TextBox();
+            TxtLast_Name = new TextBox();
+            TxtFirst_Name = new TextBox();
+            TxtCustomerId = new TextBox();
+            label10 = new Label();
+            label9 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            tabControl1.SuspendLayout();
+            tabPageCustomerList.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)DgCustomer).BeginInit();
+            tabPageCustomerDetail.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Margin = new Padding(3, 2, 3, 2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(776, 74);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Consolas", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(145, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(116, 28);
+            label1.TabIndex = 1;
+            label1.Text = "Customer";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.providers;
+            pictureBox1.Location = new Point(3, 0);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(136, 72);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPageCustomerList);
+            tabControl1.Controls.Add(tabPageCustomerDetail);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 74);
+            tabControl1.Margin = new Padding(3, 2, 3, 2);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(776, 316);
+            tabControl1.TabIndex = 1;
+            // 
+            // tabPageCustomerList
+            // 
+            tabPageCustomerList.Controls.Add(BtnClose);
+            tabPageCustomerList.Controls.Add(BtnDelete);
+            tabPageCustomerList.Controls.Add(BtnEdit);
+            tabPageCustomerList.Controls.Add(BtnNew);
+            tabPageCustomerList.Controls.Add(DgCustomer);
+            tabPageCustomerList.Controls.Add(BtnSearch);
+            tabPageCustomerList.Controls.Add(TxtSearch);
+            tabPageCustomerList.Controls.Add(label2);
+            tabPageCustomerList.Location = new Point(4, 24);
+            tabPageCustomerList.Margin = new Padding(3, 2, 3, 2);
+            tabPageCustomerList.Name = "tabPageCustomerList";
+            tabPageCustomerList.Padding = new Padding(3, 2, 3, 2);
+            tabPageCustomerList.Size = new Size(768, 288);
+            tabPageCustomerList.TabIndex = 0;
+            tabPageCustomerList.Text = "Customer List ";
+            tabPageCustomerList.UseVisualStyleBackColor = true;
+            // 
+            // BtnClose
+            // 
+            BtnClose.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            BtnClose.Image = Properties.Resources.cerrar;
+            BtnClose.Location = new Point(652, 226);
+            BtnClose.Margin = new Padding(3, 2, 3, 2);
+            BtnClose.Name = "BtnClose";
+            BtnClose.Size = new Size(99, 37);
+            BtnClose.TabIndex = 7;
+            BtnClose.UseVisualStyleBackColor = true;
+            // 
+            // BtnDelete
+            // 
+            BtnDelete.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            BtnDelete.Image = Properties.Resources.delete;
+            BtnDelete.Location = new Point(652, 172);
+            BtnDelete.Margin = new Padding(3, 2, 3, 2);
+            BtnDelete.Name = "BtnDelete";
+            BtnDelete.Size = new Size(99, 37);
+            BtnDelete.TabIndex = 6;
+            BtnDelete.UseVisualStyleBackColor = true;
+            // 
+            // BtnEdit
+            // 
+            BtnEdit.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            BtnEdit.Image = Properties.Resources.edit;
+            BtnEdit.Location = new Point(652, 123);
+            BtnEdit.Margin = new Padding(3, 2, 3, 2);
+            BtnEdit.Name = "BtnEdit";
+            BtnEdit.Size = new Size(99, 38);
+            BtnEdit.TabIndex = 5;
+            BtnEdit.UseVisualStyleBackColor = true;
+            // 
+            // BtnNew
+            // 
+            BtnNew.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            BtnNew.Image = Properties.Resources._new;
+            BtnNew.Location = new Point(652, 69);
+            BtnNew.Margin = new Padding(3, 2, 3, 2);
+            BtnNew.Name = "BtnNew";
+            BtnNew.Size = new Size(99, 42);
+            BtnNew.TabIndex = 4;
+            BtnNew.UseVisualStyleBackColor = true;
+            // 
+            // DgCustomer
+            // 
+            DgCustomer.AllowUserToAddRows = false;
+            DgCustomer.AllowUserToDeleteRows = false;
+            DgCustomer.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            DgCustomer.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            DgCustomer.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            DgCustomer.Location = new Point(28, 69);
+            DgCustomer.Margin = new Padding(3, 2, 3, 2);
+            DgCustomer.Name = "DgCustomer";
+            DgCustomer.ReadOnly = true;
+            DgCustomer.RowHeadersWidth = 51;
+            DgCustomer.Size = new Size(543, 196);
+            DgCustomer.TabIndex = 3;
+            // 
+            // BtnSearch
+            // 
+            BtnSearch.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            BtnSearch.Image = Properties.Resources.search_small;
+            BtnSearch.Location = new Point(508, 25);
+            BtnSearch.Margin = new Padding(3, 2, 3, 2);
+            BtnSearch.Name = "BtnSearch";
+            BtnSearch.Size = new Size(63, 30);
+            BtnSearch.TabIndex = 2;
+            BtnSearch.UseVisualStyleBackColor = true;
+            // 
+            // TxtSearch
+            // 
+            TxtSearch.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            TxtSearch.Location = new Point(27, 30);
+            TxtSearch.Margin = new Padding(3, 2, 3, 2);
+            TxtSearch.Name = "TxtSearch";
+            TxtSearch.PlaceholderText = "Data to search";
+            TxtSearch.Size = new Size(476, 23);
+            TxtSearch.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Consolas", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(28, 8);
+            label2.Name = "label2";
+            label2.Size = new Size(144, 19);
+            label2.TabIndex = 0;
+            label2.Text = "Search Customer";
+            // 
+            // tabPageCustomerDetail
+            // 
+            tabPageCustomerDetail.Controls.Add(BtnCancel);
+            tabPageCustomerDetail.Controls.Add(BtnSave);
+            tabPageCustomerDetail.Controls.Add(TxtEmail);
+            tabPageCustomerDetail.Controls.Add(TxtPhone);
+            tabPageCustomerDetail.Controls.Add(TxtBirthday);
+            tabPageCustomerDetail.Controls.Add(TxtAddress);
+            tabPageCustomerDetail.Controls.Add(TxtDocument);
+            tabPageCustomerDetail.Controls.Add(TxtLast_Name);
+            tabPageCustomerDetail.Controls.Add(TxtFirst_Name);
+            tabPageCustomerDetail.Controls.Add(TxtCustomerId);
+            tabPageCustomerDetail.Controls.Add(label10);
+            tabPageCustomerDetail.Controls.Add(label9);
+            tabPageCustomerDetail.Controls.Add(label8);
+            tabPageCustomerDetail.Controls.Add(label7);
+            tabPageCustomerDetail.Controls.Add(label6);
+            tabPageCustomerDetail.Controls.Add(label5);
+            tabPageCustomerDetail.Controls.Add(label4);
+            tabPageCustomerDetail.Controls.Add(label3);
+            tabPageCustomerDetail.Location = new Point(4, 24);
+            tabPageCustomerDetail.Margin = new Padding(3, 2, 3, 2);
+            tabPageCustomerDetail.Name = "tabPageCustomerDetail";
+            tabPageCustomerDetail.Padding = new Padding(3, 2, 3, 2);
+            tabPageCustomerDetail.Size = new Size(768, 288);
+            tabPageCustomerDetail.TabIndex = 1;
+            tabPageCustomerDetail.Text = "Customer Detail";
+            tabPageCustomerDetail.UseVisualStyleBackColor = true;
+            // 
+            // BtnCancel
+            // 
+            BtnCancel.Image = Properties.Resources.cancel;
+            BtnCancel.Location = new Point(228, 207);
+            BtnCancel.Margin = new Padding(3, 2, 3, 2);
+            BtnCancel.Name = "BtnCancel";
+            BtnCancel.Size = new Size(146, 55);
+            BtnCancel.TabIndex = 17;
+            BtnCancel.UseVisualStyleBackColor = true;
+            // 
+            // BtnSave
+            // 
+            BtnSave.Image = Properties.Resources.save;
+            BtnSave.Location = new Point(28, 207);
+            BtnSave.Margin = new Padding(3, 2, 3, 2);
+            BtnSave.Name = "BtnSave";
+            BtnSave.Size = new Size(146, 55);
+            BtnSave.TabIndex = 16;
+            BtnSave.UseVisualStyleBackColor = true;
+            // 
+            // TxtEmail
+            // 
+            TxtEmail.Location = new Point(412, 146);
+            TxtEmail.Margin = new Padding(3, 2, 3, 2);
+            TxtEmail.Name = "TxtEmail";
+            TxtEmail.Size = new Size(139, 23);
+            TxtEmail.TabIndex = 15;
+            // 
+            // TxtPhone
+            // 
+            TxtPhone.Location = new Point(230, 146);
+            TxtPhone.Margin = new Padding(3, 2, 3, 2);
+            TxtPhone.Name = "TxtPhone";
+            TxtPhone.Size = new Size(145, 23);
+            TxtPhone.TabIndex = 14;
+            // 
+            // TxtBirthday
+            // 
+            TxtBirthday.Location = new Point(28, 146);
+            TxtBirthday.Margin = new Padding(3, 2, 3, 2);
+            TxtBirthday.Name = "TxtBirthday";
+            TxtBirthday.Size = new Size(147, 23);
+            TxtBirthday.TabIndex = 13;
+            // 
+            // TxtAddress
+            // 
+            TxtAddress.Location = new Point(599, 83);
+            TxtAddress.Margin = new Padding(3, 2, 3, 2);
+            TxtAddress.Multiline = true;
+            TxtAddress.Name = "TxtAddress";
+            TxtAddress.Size = new Size(149, 62);
+            TxtAddress.TabIndex = 12;
+            // 
+            // TxtDocument
+            // 
+            TxtDocument.Location = new Point(412, 83);
+            TxtDocument.Margin = new Padding(3, 2, 3, 2);
+            TxtDocument.Name = "TxtDocument";
+            TxtDocument.Size = new Size(139, 23);
+            TxtDocument.TabIndex = 11;
+            // 
+            // TxtLast_Name
+            // 
+            TxtLast_Name.Location = new Point(230, 83);
+            TxtLast_Name.Margin = new Padding(3, 2, 3, 2);
+            TxtLast_Name.Name = "TxtLast_Name";
+            TxtLast_Name.Size = new Size(145, 23);
+            TxtLast_Name.TabIndex = 10;
+            // 
+            // TxtFirst_Name
+            // 
+            TxtFirst_Name.Location = new Point(28, 83);
+            TxtFirst_Name.Margin = new Padding(3, 2, 3, 2);
+            TxtFirst_Name.Name = "TxtFirst_Name";
+            TxtFirst_Name.Size = new Size(147, 23);
+            TxtFirst_Name.TabIndex = 9;
+            // 
+            // TxtCustomerId
+            // 
+            TxtCustomerId.Location = new Point(28, 33);
+            TxtCustomerId.Margin = new Padding(3, 2, 3, 2);
+            TxtCustomerId.Name = "TxtCustomerId";
+            TxtCustomerId.ReadOnly = true;
+            TxtCustomerId.Size = new Size(110, 23);
+            TxtCustomerId.TabIndex = 8;
+            TxtCustomerId.Text = "0";
+            TxtCustomerId.TextAlign = HorizontalAlignment.Right;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.Location = new Point(412, 129);
+            label10.Name = "label10";
+            label10.Size = new Size(36, 15);
+            label10.TabIndex = 7;
+            label10.Text = "Email";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.Location = new Point(599, 66);
+            label9.Name = "label9";
+            label9.Size = new Size(51, 15);
+            label9.TabIndex = 6;
+            label9.Text = "Address";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Location = new Point(230, 129);
+            label8.Name = "label8";
+            label8.Size = new Size(91, 15);
+            label8.TabIndex = 5;
+            label8.Text = "Phone Number";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(412, 66);
+            label7.Name = "label7";
+            label7.Size = new Size(115, 15);
+            label7.TabIndex = 4;
+            label7.Text = "Document Number";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(28, 129);
+            label6.Name = "label6";
+            label6.Size = new Size(54, 15);
+            label6.TabIndex = 3;
+            label6.Text = "Birthday";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(28, 66);
+            label5.Name = "label5";
+            label5.Size = new Size(67, 15);
+            label5.TabIndex = 2;
+            label5.Text = "First Name";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(230, 66);
+            label4.Name = "label4";
+            label4.Size = new Size(65, 15);
+            label4.TabIndex = 1;
+            label4.Text = "Last Name";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(28, 16);
+            label3.Name = "label3";
+            label3.Size = new Size(21, 15);
+            label3.TabIndex = 0;
+            label3.Text = " Id";
+            // 
+            // CustomerView
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(776, 390);
+            Controls.Add(tabControl1);
+            Controls.Add(panel1);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "CustomerView";
+            Text = "Customer Management";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            tabControl1.ResumeLayout(false);
+            tabPageCustomerList.ResumeLayout(false);
+            tabPageCustomerList.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)DgCustomer).EndInit();
+            tabPageCustomerDetail.ResumeLayout(false);
+            tabPageCustomerDetail.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private TabControl tabControl1;
+        private TabPage tabPageCustomerList;
+        private TabPage tabPageCustomerDetail;
+        private Button BtnSearch;
+        private TextBox TxtSearch;
+        private Label label2;
+        private DataGridView DgCustomer;
+        private Button BtnClose;
+        private Button BtnDelete;
+        private Button BtnEdit;
+        private Button BtnNew;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private TextBox TxtPhone;
+        private TextBox TxtBirthday;
+        private TextBox TxtAddress;
+        private TextBox TxtDocument;
+        private TextBox TxtLast_Name;
+        private TextBox TxtFirst_Name;
+        private TextBox TxtCustomerId;
+        private TextBox TxtEmail;
+        private Button BtnCancel;
+        private Button BtnSave;
+    }
+}
---
+++ Supermarket-mvp/Views/CustomerView.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Supermarket_mvp.Views
+{
+    public partial class CustomerView : Form, ICustomerView
+    {
+        private bool isEdit;
+        private bool isSuccessful;
+        private string message;
+        public CustomerView()
+        {
+            InitializeComponent();
+            AssociateAndRaiseViewEvents();
+
+            tabControl1.TabPages.Remove(tabPageCustomerDetail);
+            BtnClose.Click += delegate { this.Close(); };
+        }
+
+        private void AssociateAndRaiseViewEvents()
+        {
+            BtnSearch.Click += delegate { SearchEvent?.Invoke(this, EventArgs.Empty); };
+            TxtSearch.KeyDown += (s, e) =>
+            {
+                if (e.KeyCode == Keys.Enter)
+                {
+                    SearchEvent?.Invoke(this, EventArgs.Empty);
+                }
+            };
+            BtnNew.Click += delegate
+            {
+                AddNewEvent?.Invoke(this, EventArgs.Empty);
+
+                tabControl1.TabPages.Remove(tabPageCustomerList);
+                tabControl1.TabPages.Add(tabPageCustomerDetail);
+                tabPageCustomerDetail.Text = "Add New Customer";
+            };
+
+
+            BtnEdit.Click += delegate
+            {
+                EditEvent?.Invoke(this, EventArgs.Empty);
+
+                tabControl1.TabPages.Remove(tabPageCustomerList);
+                tabControl1.TabPages.Add(tabPageCustomerDetail);
+                tabPageCustomerDetail.Text = "Edit Customer";
+
+            };
+
+            BtnDelete.Click += delegate
+            {
+                var result = MessageBox.Show(
+                    "Are you sure you want to delete the selected Pay Mode",
+                    "Warning",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == DialogResult.Yes)
+                {
+                    DeleteEvent?.Invoke(this, EventArgs.Empty);
+                    MessageBox.Show(Message);
+                }
+
+            };
+            BtnSave.Click += delegate
+            {
+                SaveEvent?.Invoke(this, EventArgs.Empty);
+
+                if (isSuccessful)
+                {
+                    tabControl1.TabPages.Remove(tabPageCustomerDetail);
+                    tabControl1.TabPages.Add(tabPageCustomerList);
+                }
+                MessageBox.Show(Message);
+            };
+            BtnCancel.Click += delegate
+            {
+                CancelEvent?.Invoke(this, EventArgs.Empty);
+                tabControl1.TabPages.Remove(tabPageCustomerDetail);
+                tabControl1.TabPages.Add(tabPageCustomerList);
+            };
+
+        }
+
+        public string Customer_Id
+        {
+            get { return TxtCustomerId.Text; }
+            set { TxtCustomerId.Text = value; }
+        }
+        public string First_Name
+        {
+            get { return TxtFirst_Name.Text; }
+            set { TxtFirst_Name.Text = value; }
+        }
+        public string Last_Name
+        {
+            get { return TxtLast_Name.Text; }
+            set { TxtLast_Name.Text = value; }
+        }
+        public string Document_Number
+        {
+            get { return TxtDocument.Text; }
+            set { TxtDocument.Text = value; }
+        }
+        public string Address
+        {
+            get { return TxtAddress.Text; }
+            set { TxtAddress.Text = value; }
+        }
+        public string Birthday
+        {
+            get { return TxtBirthday.Text; }
+            set { TxtBirthday.Text = value; }
+        }
+        public string Phone_Number
+        {
+            get { return TxtPhone.Text; }
+            set { TxtPhone.Text = value; }
+        }
+        public string Email
+        {
+            get { return TxtEmail.Text; }
+            set { TxtEmail.Text = value; }
+        }
+        public string SearchValue
+        {
+            get { return TxtSearch.Text; }
+            set { TxtSearch.Text = value; }
+        }
+        public bool IsEdit
+        {
+            get { return isEdit; }
+            set { isEdit = value; }
+        }
+        public bool IsSuccessful
+        {
+            get { return isSuccessful; }
+            set { isSuccessful = value; }
+        }
+        public string Message
+        {
+            get { return message; }
+            set { message = value; }
+        }
+
+        public event EventHandler SearchEvent;
+        public event EventHandler AddNewEvent;
+        public event EventHandler EditEvent;
+        public event EventHandler DeleteEvent;
+        public event EventHandler SaveEvent;
+        public event EventHandler CancelEvent;
+
+        public void SetCustomerListBildingSource(BindingSource CustomerList)
+        {
+            DgCustomer.DataSource = CustomerList;
+        }
+
+        private static CustomerView instance;
+
+        public static CustomerView GetInstance(Form parentContainer)
+        {
+            if (instance == null || instance.IsDisposed)
+            {
+                instance = new CustomerView();
+                instance.MdiParent = parentContainer;
+
+                instance.FormBorderStyle = FormBorderStyle.None;
+                instance.Dock = DockStyle.Fill;
+            }
+            else
+            {
+                if (instance.WindowState == FormWindowState.Minimized)
+                {
+                    instance.WindowState = FormWindowState.Normal;
+                }
+                instance.BringToFront();
+            }
+            return instance;
+        }
+    }
+}
